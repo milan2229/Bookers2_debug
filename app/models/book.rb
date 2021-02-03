e2478fd90071d86181a3689ea4775e71e4b03f95
milan2229 <yuuyaneish1899@gmail.com>
2021-02-03 00:21:07 +0000

    mission3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,17 @@
 class Book < ApplicationRecord
 
 	belongs_to :user
-
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+	
+	
+	def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  	end
+  	#引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）
+  	#するかどうかを調べる
+  	
+	
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
 end
